ee850cb1acf129c2e9ccf17e8d2922e77e581d26
akowa <akowa344@gmail.com>
2024-12-03 16:19:16 -0500

    utility class.

--- src/test/java/Rest_Assured_project/Mathias/NewUser.java
+++ src/test/java/Rest_Assured_project/Mathias/NewUser.java
@@ -1,5 +1,6 @@
 package Rest_Assured_project.Mathias;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
 import org.mozilla.javascript.Token;
 import org.testng.annotations.Test;
@@ -14,14 +15,14 @@ public class NewUser {
     @Test
     public static void newUser() {
         HashMap<String, String> map = new HashMap<>();
-        map.put("name", randomUserName() +" Akowanou");
+        map.put("name", randomUserName() +" Mathias");
         map.put("gender", "male");
         map.put("email", randomEmail());
         map.put("status", "active");
         String token ="Bearer e0ad33a27af2be5ef6fb60647c1f76fba10258b0373027c39f9834d16f10c821";
 
                  given().header("Authorization", token)
-                         .contentType("application/json")
+                         .contentType(ContentType.JSON)
                          .body(map)
                 .when()
                 .post("https://gorest.co.in/public/v2/users")
