562b1c3222e0d176002fe6992005a3ee10dbd8bb
akowa <akowa344@gmail.com>
2024-12-03 16:42:20 -0500

    random update

--- src/test/java/Rest_Assured_project/Mathias/NewUser.java
+++ src/test/java/Rest_Assured_project/Mathias/NewUser.java
@@ -10,14 +10,17 @@ import java.util.HashMap;
 import static Rest_Assured_project.UtilityClass.randomEmail;
 import static Rest_Assured_project.UtilityClass.randomUserName;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class NewUser {
     @Test
     public static void newUser() {
+        String randomUserName=randomUserName()+" Mathias";
+        String randomEmail = randomEmail();
         HashMap<String, String> map = new HashMap<>();
-        map.put("name", randomUserName() +" Mathias");
+        map.put("name", randomUserName);
         map.put("gender", "male");
-        map.put("email", randomEmail());
+        map.put("email", randomEmail);
         map.put("status", "active");
         String token ="Bearer e0ad33a27af2be5ef6fb60647c1f76fba10258b0373027c39f9834d16f10c821";
 
@@ -27,6 +30,6 @@ public class NewUser {
                 .when()
                 .post("https://gorest.co.in/public/v2/users")
                 .then().statusCode(201)
-                .log().all();
+                .body("name",equalTo(randomUserName)).log().all();
     }
 }
