dd93a871658daf6eb10fe82844647a91c972923f
akowa <akowa344@gmail.com>
2024-12-04 16:06:52 -0500

    random update1

--- src/test/java/Rest_Assured_project/Mathias/NewUser.java
+++ src/test/java/Rest_Assured_project/Mathias/NewUser.java
@@ -1,6 +1,7 @@
 package Rest_Assured_project.Mathias;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import org.mozilla.javascript.Token;
 import org.testng.annotations.Test;
@@ -23,7 +24,6 @@ public class NewUser {
         map.put("email", randomEmail);
         map.put("status", "active");
         String token ="Bearer e0ad33a27af2be5ef6fb60647c1f76fba10258b0373027c39f9834d16f10c821";
-
                  given().header("Authorization", token)
                          .contentType(ContentType.JSON)
                          .body(map)
