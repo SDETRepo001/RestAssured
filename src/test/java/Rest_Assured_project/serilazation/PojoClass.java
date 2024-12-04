a57af37bb18ab8f578eb9aa22b4ff9c7f6581f64
Hamid-Post <hamidiabdul@gmail.com>
2024-12-04 12:18:30 -0500

    new Implementation ver001

---
+++ src/test/java/Rest_Assured_project/serilazation/PojoClass.java
@@ -0,0 +1,35 @@
+package Rest_Assured_project.serilazation;
+
+public class PojoClass {
+
+    String name;
+    String lastName;
+    String class_name;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getClass_name() {
+        return class_name;
+    }
+
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setClass_name(String class_name) {
+        this.class_name = class_name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
