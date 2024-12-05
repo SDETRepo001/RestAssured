f63ffa744425b2573cba841ee2c6a2998c39e7e3
Hamid-Post <hamidiabdul@gmail.com>
2024-12-05 15:12:27 -0500

    API Chaining practice one

--- src/test/java/Rest_Assured_project/SchemaValidator/JSonValidator.java
+++ src/test/java/Rest_Assured_project/SchemaValidator/JSonValidator.java
@@ -5,7 +5,7 @@ import org.json.JSONObject;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+//import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
 
 import static io.restassured.RestAssured.given;
@@ -17,8 +17,8 @@ public class JSonValidator {
             // Send the GET request
           given()
                     .when()
-                    .get("https://fakerestapi.azurewebsites.net/api/v1/Activities")
-                    .then().assertThat().body(matchesJsonSchemaInClasspath("JsonSchemafile.json")); // Add the closing parenthesis and the file extension
+                    .get("https://fakerestapi.azurewebsites.net/api/v1/Activities");
+//                    .then().assertThat().body(matchesJsonSchemaInClasspath("JsonSchemafile.json")); // Add the closing parenthesis and the file extension
         }
     }
 
